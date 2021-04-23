3bbe594e401af117f3c7e9cccf0243d6d56a8ee1
Garrett Pederson <pedersongw@gmail.com>
2021-04-23 15:51:08 -0400

    added four tab sections with navigation buttons. All have display set to none inline. A javascript function sets the display for the first tab so it is visibible upon load.

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-document.body.style.backgroundColor = "lightblue";
+window.onload = function() {
+    document.getElementById("tab1").style.display = "block";
+    document.getElementById("tab-holder1").style.display = "flex";
+}
+/*
+
+function goToTab1() {
+return true;
+}
+function goToTab2() {
+return true;
+
+
+}
+function goToTab3() {
+return true;
+}
+function goToTab4() {
+   return true;
+
+}
+window.onclick = function(event) {
+    let clicked = Number(event.target.id.slice(-3));
+    if(clicked == 1.1 ||clicked == 2.1 ||clicked == 3.1 ||clicked == 4.1) {
+        goToTab1();
+    } else if(clicked == 1.2 ||clicked == 2.2 ||clicked == 3.2 ||clicked == 4.2) {
+        goToTab2();
+    } else if(clicked == 1.3 ||clicked == 2.3 ||clicked == 3.3 ||clicked == 4.3) {
+        goToTab3();
+    } else if(clicked == 1.4 ||clicked == 2.4 ||clicked == 3.4 ||clicked == 4.4) { 
+        goToTab4();
+    }
+}
+*/
